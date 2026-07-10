a5116a6991c0e8ddd07e6768f18da82a3ba6e517
kokonamarumoto <kokona.marumoto@nsw.co.jp>
2026-07-10 16:13:58 +0900

    Fix ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-$t&ma2o9x*9m)cx3n-gk1%zjc!y)ywdm@x)ea7n26awab#66#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+"localhost",
+    "127.0.0.1",
+    ".vercel.app",
+]
 
 
 # Application definition
